35d11e0c73a6d204130f63246df6e6bc3f001c57
Donghyun Seo <donghyun.seo91@gmail.com>
2021-07-15 16:29:49 +0900

    fix: Fix array bug

--- node/packages/core/scripts/schema.js
+++ node/packages/core/scripts/schema.js
@@ -11,8 +11,9 @@ async function generate(targetPath) {
   const schemaMap = new Map();
 
   for (const filePath of filePaths) {
-    const dirPath = path.dirname(filePath);
-    const dirsName = dirPath.replace(schemaPath, '').split(path.sep);
+    const absoluteDirPath = path.dirname(filePath);
+    const relativeDirPath = absoluteDirPath.replace(schemaPath, '');
+    const dirsName = relativeDirPath.replace(schemaPath, '').split(path.sep);
     const dirName = dirsName[dirsName.length - 1];
     const fileName = path.basename(filePath).split('.').shift();
     const dereferenced = await refParser.dereference(filePath, {
@@ -22,7 +23,9 @@ async function generate(targetPath) {
     const schema = {
       dereferenced,
       filePath,
-      dirPath,
+      absoluteDirPath,
+      relativeDirPath,
+      dirsName,
       dirName,
       fileName,
     };
@@ -43,37 +46,21 @@ async function generate(targetPath) {
       let imports = [];
 
       if (schema.dereferenced.extends) {
-        imports = imports.concat(
-          getImports(schema.dereferenced.extends, schema, schemaMap)
-        );
+        imports = imports.concat(getImports(schema.dereferenced.extends, schema, schemaMap));
       }
 
       if (schema.dereferenced.properties) {
-        imports = imports.concat(
-          getImports(
-            Object.values(schema.dereferenced.properties),
-            schema,
-            schemaMap
-          )
-        );
+        imports = imports.concat(getImports(Object.values(schema.dereferenced.properties), schema, schemaMap));
       }
 
-      imports = imports.filter(
-        (v, i, a) => a.findIndex((v2) => v.title === v2.title) === i
-      );
+      imports = imports.filter((v, i, a) => a.findIndex(v2 => v.title === v2.title) === i);
 
       for (const item of imports) {
         ts = `import { ${item.title} } from '${item.path}';\n` + ts;
       }
 
-      await fs.copy(
-        schema.filePath,
-        `${targetPath}/json/${schema.dirName || ''}/${schema.fileName}.json`
-      );
-      await fs.outputFile(
-        `${targetPath}/types/${schema.dirName || ''}/${schema.fileName}.ts`,
-        ts
-      );
+      await fs.copy(schema.filePath, `${targetPath}/json/${schema.relativeDirPath || ''}/${schema.fileName}.json`);
+      await fs.outputFile(`${targetPath}/types/${schema.relativeDirPath || ''}/${schema.fileName}.ts`, ts);
     } catch (e) {
       console.log(e);
     }
@@ -84,27 +71,27 @@ function getImports(dereferenced, schema, schemaMap) {
   const imports = [];
 
   if (Array.isArray(dereferenced)) {
-    dereferenced.forEach((item) => {
-      if (!item.title) {
-        return;
+    dereferenced.forEach(item => {
+      if (item.type === 'object' && typeof item.title !== 'undefined') {
+        imports.push({
+          title: item.title,
+          path: getRelative(schema.absoluteDirPath, schemaMap.get(item.title).absoluteDirPath, item.title),
+        });
+      } else if (item.type === 'array' && typeof item.items.title !== 'undefined') {
+        imports.push({
+          title: item.items.title,
+          path: getRelative(schema.absoluteDirPath, schemaMap.get(item.items.title).absoluteDirPath, item.items.title),
+        });
       }
-      imports.push({
-        title: item.title,
-        path: getRelative(
-          schema.dirPath,
-          schemaMap.get(item.title).dirPath,
-          item.title
-        ),
-      });
     });
   } else if (typeof dereferenced === 'object') {
     if (dereferenced.title) {
       imports.push({
         title: dereferenced.title,
         path: getRelative(
-          schema.dirPath,
-          schemaMap.get(dereferenced.title).dirPath,
-          dereferenced.title
+          schema.absoluteDirPath,
+          schemaMap.get(dereferenced.title).absoluteDirPath,
+          dereferenced.title,
         ),
       });
     }
